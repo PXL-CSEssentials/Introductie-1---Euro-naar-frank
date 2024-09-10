7ce48d8fc15942a9d21b9cc965fea8fb1745ffd8
Koen Bloemen <koen.bloemen@pxl.be>
2024-09-10 08:53:17 +0200

    Update namespace

--- Introductie-1---Euro-naar-frank/MainWindow.xaml.cs
+++ Introductie-1---Euro-naar-frank/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EurToBef;
+namespace Introductie_1___Euro_naar_frank;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
